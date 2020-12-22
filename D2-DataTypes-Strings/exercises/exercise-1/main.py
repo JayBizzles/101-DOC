7c7447f3d6cfbaab85a0e1f29833e9fa311194f2
JayBizzles <jbills@ucmerced.edu>
2020-12-22 01:14:10 -0800

    forgot these

---
+++ D2-DataTypes-Strings/exercises/exercise-1/main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+#input is 26
+
+print(type(two_digit_number))
+
+#now I know that this is a string type
+
+#So I should be able to subscript
+
+int1 = int(two_digit_number[0])
+
+int2 = int(two_digit_number[1])
+
+print(int1 + int2)
